8908f29a3c54e7d0be343bf806a3d00b7d914de1
Cheasy <148623628+ChairulR@users.noreply.github.com>
2024-11-21 14:53:23 +0700

    Add files via upload

---
+++ C.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    char name[20];
+    int age;
+
+    printf("Please, Enter your name :");
+    scanf ("&s", &name);
+    
+    printf ("Plese, Enter your age :");
+    scanf("&s", &age);
+
+    return 0;
+    }
